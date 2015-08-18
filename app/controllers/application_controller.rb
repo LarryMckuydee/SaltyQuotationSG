963a6304e92a32e5395bcbe39c427b351d053855
LarryMckuydee <larry.mckuydee@gmail.com>
2015-08-18 11:27:47 +0800

    fix for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def authenticate_user!
@@ -11,6 +12,12 @@ class ApplicationController < ActionController::Base
       redirect_to new_apparel_consultant_session, :notice => 'Please login to proceed'
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit :name, :email, :password, :password_confirmation
     end
   end
 end
